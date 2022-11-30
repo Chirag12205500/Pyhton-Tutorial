d4b773d8c059f8d81ada977239374cd7b4a35a53
Chirag Sharma <csharma2923@gmail.com>
2022-11-30 23:43:28 +0530

    Added-Tutorial

---
+++ Python Tutorial.py
@@ -0,0 +1,15 @@
+a = 10
+print(a)
+a = "I love python"
+print(a)
+a = 20.25
+print("float value of variable is :", a)
+print("Hello""\n""World")
+a = b = c = 10
+print(a)
+print(b)
+print(c)
+value1 = value2 = value3 = 99, "hello world", "Hello World"
+print(a)
+print(b)
+print(c)
